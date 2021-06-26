2b3ff3586c6360247555a22b3ae60de5c19a6009
Diana Burkart <diana.burkart@web.de>
2021-06-26 17:11:13 +0200

    add Detail Screen image and description table

--- lib/DetailScreen.dart
+++ lib/DetailScreen.dart
@@ -10,36 +10,67 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  alignment: Alignment.center,
-                  child: Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 2)
-                      ),
-                      child: Image.asset(
-                        imagePath,
-                        fit: BoxFit.fitHeight,
-                      ),
-                    )
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: Center(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 2)
+                          ),
+                          child: Image.asset(
+                            imagePath,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        )
+                    ),
+                    constraints: BoxConstraints(
+                      maxHeight: MediaQuery
+                          .of(context)
+                          .size
+                          .height * 0.4,
+                    ),
                   ),
-                  constraints: BoxConstraints(
-                    maxHeight: MediaQuery.of(context).size.height * 0.4,
-                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 2)
                 ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        color: Colors.lightBlue,
+                        child: Column(
+                          children: [
+                            Text("Hallo")
+                          ]
+                        ),
+                      )
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.lightGreen,
+                        child: Column(
+                          children: [
+                            Text("Hallo")
+                          ]
+                        ),
+                      )
+                    )
+                  ]
+                )
               )
-            ],
-          ),
-          Row(
-            children: [
-              Text("Second row")
-            ],
-          ),
-        ],
+            )
+          ]
       )
     );
   }
