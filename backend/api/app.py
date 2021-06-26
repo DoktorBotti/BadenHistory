ab637c3ed6140cd076641af10f8bef2b5a080d02
Sebastian Weber <uwmgy@student.kit.edu>
2021-06-26 13:29:19 +0200

    added api endpoint to get and post voice, added insert script

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, render_template, send_file
 import sqlite3 as sqlite
-import sys
 import os
+import shutil
 app = Flask(__name__)
 
 
@@ -40,11 +40,23 @@ def api_get_in_bounds():
     elements_in_bounds = cur.execute(build_get_in_bounds(query_parameters)).fetchall()
     return jsonify(elements_in_bounds)
 
-@app.route('/api/images/', methods=['GET'])
-def api_get_image_by_id():
-    query_parameters = request.args
-    image = query_parameters.get("image")
+@app.route('/api/images/<id>/', methods=['GET'])
+def api_get_image_by_id(id):
+    conn = sqlite.connect('../data/elements.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    image = cur.execute("SELECT image FROM elements WHERE id=" + id + ";").fetchall()[0].get("image")
+    print(image)
     return send_file("../data/" + image + ".jpg", mimetype='image/gif')
+
+@app.route('/api/voices/<id>/', methods=['GET'])
+def api_get_voice_by_id(id):
+    conn = sqlite.connect('../data/elements.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    voice = cur.execute("SELECT voice FROM elements WHERE id=" + id + ";").fetchall()[0].get("voice")
+    print(voice)
+    return send_file("../data/" + voice + ".wav", as_attachment=True)
 
 @app.route('/api/elements/insert/', methods=['GET'])
 def api_insert():
@@ -53,7 +65,35 @@ def api_insert():
     conn.row_factory = dict_factory
     cur = conn.cursor()
     insert = build_insert(query_parameters)
-    cur.execute(insert)
+    id = cur.execute(insert).lastrowid
+    conn.commit()
+    return jsonify({"id":id})
+
+@app.route('/api/elements/<id>/upload_image/', methods=['POST'])
+def api_upload_image(id):
+    conn = sqlite.connect('../data/elements.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    file = request.files.get("file")
+    imdasid = file.filename
+    with open("../data/"+str(imdasid)+".jpg", 'wb') as f:
+        shutil.copyfileobj(file, f)
+
+    cur.execute("UPDATE elements SET image=\""+imdasid+"\" WHERE id="+id+";")
+    conn.commit()
+    return "Inserted object"
+
+@app.route('/api/elements/<id>/upload_voice/', methods=['POST'])
+def api_upload_voice(id):
+    conn = sqlite.connect('../data/elements.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    file = request.files.get("file")
+    filename = file.filename
+    with open("../data/"+filename+".wav", 'wb') as f:
+        shutil.copyfileobj(file, f)
+
+    cur.execute("UPDATE elements SET voice="+filename+" WHERE id="+id+";")
     conn.commit()
     return "Inserted object"
 
