3c0c89d3be745d99170e5af244b44af1c32fe5cc
Thomas Weber <tom.werm@gmail.com>
2021-06-26 16:04:41 +0200

    Stuff

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart' as local;
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 void main() {
@@ -71,8 +71,9 @@ class _MapContainerState extends State<MapContainer> {
   @override
   void initState() {
     var fc = FetchContent();
-    Future<Record> r1 = fc.fetchRecord(1);
-    r1.then((value) => value.printDebug1());
+    fc.syncData();
+    // Future<Record> r1 = fc.fetchRecord(1);
+    // r1.then((value) => value.printDebug1());
     _ourMarkers = objectsNearby
         .map((point) =>
         Marker(
@@ -159,16 +160,51 @@ class FetchContent {
   double longitude_min = 8.33;
   double longitude_max = 8.47;
 
-  Future<Record> fetchRecord(final int id) async{
-    final response = await http.get(Uri.parse('http://192.168.178.37:5000/api/elements/?id='+id.toString()));
+  void syncData() async {
+    final response = await http.get(Uri.parse('http://192.168.178.37:5000/api/ids'));
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.
-      return Record.fromJson(jsonDecode(response.body)[0]);
+      List<dynamic> content = jsonDecode(response.body);
+      for(Map<String, dynamic> id in content) {
+        print("Looking at id " + id.values.first.toString());
+        Record? record;
+        var local = false;
+        await Record.get(id.values.first).then((value) {record=value;local=true;}).onError((error, stackTrace) {print(error);});
+        if(record == null) {
+          await fetchRecord(id.values.first).then((value) => record=value).onError((error, stackTrace) {print(error);});
+        }
+        print(local.toString());
+        if(record != null) {
+          if(!local) {
+            record!.save();
+            print("saved record " + id.values.first.toString());
+          }
+        }
+      }
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
       throw Exception('Failed to load record');
+    }
+  }
+
+  Future<Record?> fetchRecord(final int id) async{
+    final response = await http.get(Uri.parse('http://192.168.178.37:5000/api/elements/?id='+id.toString()));
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      final content = jsonDecode(response.body);
+      print(content.toString());
+      try {
+        return Record.fromJson(content[0]);
+      } catch(e) {
+        return new Future<Record>.error(id.toString() + " not found on server or parsing error");
+      }
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      return new Future<Record>.error("server response != 200");
     }
   }
 }
@@ -178,12 +214,16 @@ class Record {
   final double x;
   final double y;
   final String? image;
+  final String title;
   final String text;
+  final String place;
+  final double latitude;
+  final double longitude;
   final String? voice;
   final String type;
   final String? username;
 
-  const Record({required this.id, required this.x, required this.y, required this.image, required this.text, required this.voice, required this.type, required this.username});
+  const Record({required this.title, required this.place, required this.latitude, required this.longitude, required this.id, required this.x, required this.y, required this.image, required this.text, required this.voice, required this.type, required this.username});
 
   factory Record.fromJson(Map<String, dynamic> json) {
     return Record(
@@ -191,13 +231,49 @@ class Record {
       x: json['x'],
       y: json['y'],
       image: json['image'],
+      title: json['title'],
       text: json['text'],
+      place: json['place'],
+      latitude: json['latitude'],
+      longitude: json['longitude'],
       voice: json['voice'],
       type: json['type'],
       username: json['username']
     );
   }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id' : id,
+      'x':x ,
+      'y':y ,
+      'image':image ,
+      'title':title ,
+      'text':text ,
+      'place':place ,
+      'latitude':latitude ,
+      'longitude':longitude ,
+      'voice':voice ,
+      'type':type ,
+      'username':username
+    };
+  }
+
+  void save() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(id.toString(), jsonEncode(this));
+  }
+
+  static Future<Record> get(final int id) async {
+    final prefs = await SharedPreferences.getInstance();
+    final result = prefs.get(id.toString());
+    if(result != null) {
+      return Record.fromJson(jsonDecode(result.toString()));
+    }
+    return new Future<Record>.error("no local value found");
+  }
+
   void printDebug1() {
-    print(id.toString() + x.toString() + " " + y.toString() + (image?.toString() ?? "") + text + (voice?.toString() ?? "") + type + (username?.toString() ?? "") );
+    print(id.toString() + " " + x.toString() + " " + y.toString()+ " "  + (image?.toString() ?? "")+ " "  + text+ " "  + (voice?.toString() ?? "") + " " + type+ " "  + (username?.toString() ?? "") );
   }
 }
