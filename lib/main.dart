818b6f6e2e38bcca9d514e348fda95f57b666ab9
Thomas Weber <tom.werm@gmail.com>
2021-06-27 11:43:09 +0200

    Removed other screens, add chat WIP

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 5,
+        length: 2,
         initialIndex: 1,
         child: Scaffold(
           appBar: AppBar(
@@ -48,12 +48,12 @@ class _HomeScreenState extends State<HomeScreen> {
                   icon: Icon(Icons.favorite),
                 ),
                 Tab(text: 'Map', icon: Icon(Icons.map_rounded)),
-                Tab(
-                  text: 'Quests',
-                  icon: Icon(Icons.access_time),
-                ),
-                Tab(text: 'AudioStuff TMP', icon: Icon(Icons.access_alarms)),
-                Tab(text: 'Chat', icon: Icon(Icons.access_alarms))
+                // Tab(
+                //   text: 'Quests',
+                //   icon: Icon(Icons.access_time),
+                // ),
+                // Tab(text: 'AudioStuff TMP', icon: Icon(Icons.access_alarms)),
+                // Tab(text: 'Chat', icon: Icon(Icons.access_alarms))
               ],
             ),
           ),
@@ -61,14 +61,14 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               FindingsScreen(),
               MapContainer(),
-              DetailScreen(
-                location: "Karlsruhe",
-                imagePath: "assets/testimage.jpg",
-                title: "Exponat Nr. 15",
-                description: "Das ist Exponat Nr. 15, Lorem ipsum usw.",
-              ),
-              AudioGui(),
-              ChatDetailPage()
+              // DetailScreen(
+              //   location: "Karlsruhe",
+              //   imagePath: "assets/testimage.jpg",
+              //   title: "Exponat Nr. 15",
+              //   description: "Das ist Exponat Nr. 15, Lorem ipsum usw.",
+              // ),
+              // AudioGui(),
+              // ChatDetailPage()
             ],
             physics: NeverScrollableScrollPhysics(),
           ),
@@ -84,183 +84,115 @@ class FindingsScreen extends StatefulWidget {
 }
 
 class _FindingsScreenState extends State<FindingsScreen> {
+  // 0 DetailsScreen
+  // 1 StandardScreen
+  // 2 ChatScreen
 
-  bool _details = false;
+  int state1 = 0;
   int selected = 0;
+  void setState1(int state) {
+    setState(() {
+      state1 = state;
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-
     return Center(
         child: FutureBuilder<List<RecordViewData>>(
       future: getVisitedRecords(),
       builder: (context, rec) {
         if (rec.hasData) {
-          if(_details) {
-            //////////////////////////////////////////////////////
-            /////////////////Details Screen///////////////////////
-            //////////////////////////////////////////////////////
-            return Container(
-              child: Column(
-                  children: [
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            height: 0.1 * MediaQuery.of(context).size.height,
-                            alignment: Alignment.center,
-                            child: Text(
-                                rec.data![selected].baseRecord.title!
-                            ),
-                          ),
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text(
-                              (rec.data![selected].baseRecord.place != null ? rec.data![selected].baseRecord.place : "")!,
-                              style: TextStyle(
-                                  color: Colors.black.withOpacity(0.6)),
-                            ),
-                          ),
-                        ]
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+          if(state1 == 0) {
+            return DetailScreen(id: rec.data![selected].baseRecord.id, title: rec.data![selected].baseRecord.title!, location: rec.data![selected].baseRecord.place!, latitude: rec.data![selected].baseRecord.latitude!, longitude: rec.data![selected].baseRecord.longitude!, imagePath: rec.data![selected].path, description: rec.data![selected].baseRecord.text!);
+          } else if (state1 == 1) {
+            return ListView.builder(
+                itemCount: rec.data!.length,
+                itemBuilder: (context, i) {
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    child: Column(
                       children: [
-                        Container(
-                          constraints: BoxConstraints(
-                            maxHeight: 0.4 * MediaQuery.of(context).size.height,
-                          ),
-                          child: Image.network(rec.data![selected].path),
-                        ),
+                        // ListTile(
+                        //   leading: Icon(Icons.arrow_drop_down_circle),
+                        //   title: Text(rec.data![i].baseRecord.title),
+                        //   subtitle: Text(rec.data![i].baseRecord.place,
+                        //     style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                        //   ),
+                        // ),
+                        //rec.data![i].path,
+                        ExpansionTile(
+                          // leading:
+                            title: Row(children: [
+                              Container(
+                                height: 0.1 * MediaQuery.of(context).size.height,
+                                width: 0.3 *
+                                    MediaQuery.of(context).size.width, // 20%
+                                child: Image.network(rec.data![i].path,
+                                    fit: BoxFit.fitHeight),
+                                alignment: Alignment.center,
+                              ),
+                              Container(
+                                height: 0.1 * MediaQuery.of(context).size.height,
+                                width: 0.5 *
+                                    MediaQuery.of(context).size.width, // 60%
+                                alignment: Alignment.centerLeft,
+                                child: Text(rec.data![i].baseRecord.title!),
+                              ),
+                            ]),
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  rec.data![i].baseRecord.place!,
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ),
+                              Image.network(rec.data![i].path),
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: Text(
+                                  rec.data![i].baseRecord.text!,
+                                  style: TextStyle(
+                                      color: Colors.black.withOpacity(0.6)),
+                                ),
+                              ),
+                              ButtonBar(
+                                alignment: MainAxisAlignment.center,
+                                children: [
+
+                                  TextButton(
+                                    onPressed: () {
+                                      // DefaultTabController.of(context)!.animateTo(2);
+                                      // DefaultTabController.of(context)
+                                      // _HomeScreenState.tabController.animateTo(DetailScreen(location: rec.data![i].baseRecord.place, imagePath: rec.data![i].path,title: rec.data![i].baseRecord.title!, description: rec.data![i].baseRecord.text!,));
+                                      setState(() {
+                                        state1 = 0;
+                                      });
+                                      selected = i;
+                                      build(context);
+                                      print("pressed, switch to details");
+                                      // Navigator.push(
+                                      //   context,
+                                      //   MaterialPageRoute(builder: (context) => DetailScreen(location: rec.data![i].baseRecord.place, imagePath: rec.data![i].path,title: rec.data![i].baseRecord.title!, description: rec.data![i].baseRecord.text!,)),
+                                      // );
+                                    },
+                                    child: const Text('Details'),
+                                  ),
+                                ],
+                              ),
+                            ]),
                       ],
                     ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child: Text(
-                              (rec.data![selected].baseRecord.text != null ? rec.data![selected].baseRecord.text : "")!,
-                              style: TextStyle(
-                                  color: Colors.black.withOpacity(0.6)),
-                            ),
-                          ),
-                        ]
-                    ),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ButtonBar(
-                            alignment: MainAxisAlignment.center,
-                            children: [
-                              TextButton(
-                                onPressed: () {
-                                  // Perform some action
-
-                                  setState(() {
-                                    _details = false;
-                                  });
-                                  build(context);
-                                  print("pressed back");
-                                },
-                                child: const Text('Back'),
-                              ),
-                            ],
-                          ),
-                        ]
-                    )
-                  ]
-              ),
-            );
-            //////////////////////////////////////////////////////
-            /////////////////Details Screen///////////////////////
-            //////////////////////////////////////////////////////
+                  );
+                });
           }
-          return ListView.builder(
-              itemCount: rec.data!.length,
-              itemBuilder: (context, i) {
-                return Card(
-                  clipBehavior: Clip.antiAlias,
-                  child: Column(
-                    children: [
-                      // ListTile(
-                      //   leading: Icon(Icons.arrow_drop_down_circle),
-                      //   title: Text(rec.data![i].baseRecord.title),
-                      //   subtitle: Text(rec.data![i].baseRecord.place,
-                      //     style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      //   ),
-                      // ),
-                      //rec.data![i].path,
-                      ExpansionTile(
-                          // leading:
-                          title: Row(children: [
-                            Container(
-                              height: 0.1 * MediaQuery.of(context).size.height,
-                              width: 0.3 *
-                                  MediaQuery.of(context).size.width, // 20%
-                              child: Image.network(rec.data![i].path,
-                                  fit: BoxFit.fitHeight),
-                              alignment: Alignment.center,
-                            ),
-                            Container(
-                              height: 0.1 * MediaQuery.of(context).size.height,
-                              width: 0.5 *
-                                  MediaQuery.of(context).size.width, // 60%
-                              alignment: Alignment.centerLeft,
-                              child: Text(rec.data![i].baseRecord.title!),
-                            ),
-                          ]),
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text(
-                                rec.data![i].baseRecord.place!,
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                            Image.network(rec.data![i].path),
-                            Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Text(
-                                rec.data![i].baseRecord.text!,
-                                style: TextStyle(
-                                    color: Colors.black.withOpacity(0.6)),
-                              ),
-                            ),
-                            ButtonBar(
-                              alignment: MainAxisAlignment.center,
-                              children: [
+          else  {
+            return ChatDetailPage();
+          }
 
-                                TextButton(
-                                  onPressed: () {
-                                    // DefaultTabController.of(context)!.animateTo(2);
-                                    // DefaultTabController.of(context)
-                                    // _HomeScreenState.tabController.animateTo(DetailScreen(location: rec.data![i].baseRecord.place, imagePath: rec.data![i].path,title: rec.data![i].baseRecord.title!, description: rec.data![i].baseRecord.text!,));
-                                    setState(() {
-                                      _details = true;
-                                    });
-                                    selected = i;
-                                    build(context);
-                                    print("pressed, switch to details");
-                                    // Navigator.push(
-                                    //   context,
-                                    //   MaterialPageRoute(builder: (context) => DetailScreen(location: rec.data![i].baseRecord.place, imagePath: rec.data![i].path,title: rec.data![i].baseRecord.title!, description: rec.data![i].baseRecord.text!,)),
-                                    // );
-                                  },
-                                  child: const Text('Details'),
-                                ),
-                              ],
-                            ),
-                          ]),
-                    ],
-                  ),
-                );
-              });
         } else {
           return Card(
             child: Container(
